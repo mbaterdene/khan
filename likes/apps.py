9e562af9627cde0f96e114812d8842155e30d889
Bat-Erdene Munkhsaikhan <62886625+mbaterdene@users.noreply.github.com>
2024-10-16 14:12:03 +0800

    de

--- likes/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class LikesConfig(AppConfig):
-    name = 'likes'
-
-    def ready(self):
-        super(LikesConfig, self).ready()
-        from . import signals
